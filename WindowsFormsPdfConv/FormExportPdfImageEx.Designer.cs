b5bda0027d4f359732b68f1ad4b920a4ffb0d33e
sss <sss727@yahoo.co.jp>
2020-07-12 13:24:15 +0900

    first commit

---
+++ WindowsFormsPdfConv/FormExportPdfImageEx.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsPdfConv
+{
+    partial class FormExportPdfImageEx
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDirPath = new System.Windows.Forms.TextBox();
+            this.buttonExportPdfImage = new System.Windows.Forms.Button();
+            this.listBoxExpPdfImg = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBoxDirPath
+            // 
+            this.textBoxDirPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDirPath.Location = new System.Drawing.Point(14, 12);
+            this.textBoxDirPath.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBoxDirPath.Name = "textBoxDirPath";
+            this.textBoxDirPath.Size = new System.Drawing.Size(552, 25);
+            this.textBoxDirPath.TabIndex = 12;
+            this.textBoxDirPath.Text = "C:\\tmp";
+            // 
+            // buttonExportPdfImage
+            // 
+            this.buttonExportPdfImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonExportPdfImage.Location = new System.Drawing.Point(14, 762);
+            this.buttonExportPdfImage.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.buttonExportPdfImage.Name = "buttonExportPdfImage";
+            this.buttonExportPdfImage.Size = new System.Drawing.Size(552, 73);
+            this.buttonExportPdfImage.TabIndex = 14;
+            this.buttonExportPdfImage.Text = "Pdf 画像抽出";
+            this.buttonExportPdfImage.UseVisualStyleBackColor = true;
+            this.buttonExportPdfImage.Click += new System.EventHandler(this.buttonExportPdfImage_Click);
+            // 
+            // listBoxExpPdfImg
+            // 
+            this.listBoxExpPdfImg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxExpPdfImg.FormattingEnabled = true;
+            this.listBoxExpPdfImg.ItemHeight = 18;
+            this.listBoxExpPdfImg.Location = new System.Drawing.Point(14, 50);
+            this.listBoxExpPdfImg.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.listBoxExpPdfImg.Name = "listBoxExpPdfImg";
+            this.listBoxExpPdfImg.Size = new System.Drawing.Size(552, 706);
+            this.listBoxExpPdfImg.TabIndex = 11;
+            // 
+            // FormExportPdfImageEx
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 840);
+            this.Controls.Add(this.buttonExportPdfImage);
+            this.Controls.Add(this.textBoxDirPath);
+            this.Controls.Add(this.listBoxExpPdfImg);
+            this.Name = "FormExportPdfImageEx";
+            this.Text = "Export Pdf Image Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxDirPath;
+        private System.Windows.Forms.Button buttonExportPdfImage;
+        private System.Windows.Forms.ListBox listBoxExpPdfImg;
+    }
+}
+
---
+++ WindowsFormsPdfConv/FormExportPdfImageEx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+//NUGETから「GhostscriptSharp」をインストール
+using GhostscriptSharp;
+
+namespace WindowsFormsPdfConv
+{
+    public partial class FormExportPdfImageEx : Form
+    {
+        public FormExportPdfImageEx()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExportPdfImage_Click(object sender, EventArgs e)
+        {
+            //ディレクトリ内のファイル一覧を取得
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(textBoxDirPath.Text);
+            System.IO.FileInfo[] files = di.GetFiles("*.pdf", System.IO.SearchOption.AllDirectories);
+            foreach (System.IO.FileInfo f in files)
+            {
+                try
+                {
+                    string fileName = f.Name;
+                    string dirPass = textBoxDirPath.Text;
+                    string filePass = dirPass + fileName;
+
+                    //画像を抽出
+                    string imgFileName = getImageFromPdf(dirPass, fileName);
+                }
+                catch (Exception exp)
+                {
+                    outLogMsg("-", "Pdfファイル情報読み取り時にエラー発生 : " + exp.Message);
+                }
+
+            }
+        }
+
+        //Pdfから画像を取得する
+        private string getImageFromPdf(string baseFilePass, string pdfFielName)
+        {
+            //拡張子無しでファイル名を取得する
+            string imgFileName = Path.GetFileNameWithoutExtension(pdfFielName) + ".png";
+
+            try
+            {
+                //Pdfから画像を取得
+                GhostscriptSettings settings = new GhostscriptSettings();
+                settings.Page.AllPages = true; //全ページ出力
+                settings.Page.Start = 1;       //出力開始ページ
+                settings.Page.End = 1;         //出力終了ページ
+                settings.Size.Native = GhostscriptSharp.Settings.GhostscriptPageSizes.a0; //出力サイズ指定
+                settings.Device = GhostscriptSharp.Settings.GhostscriptDevices.png256; //出力ファイルフォーマット指定(pngで指定)
+                settings.Resolution = new Size(600, 600); //出力Dpi
+
+                string inputPath = baseFilePass + "\\" + pdfFielName;
+                string outputPath = baseFilePass + "\\" + imgFileName;
+                GhostscriptWrapper.GenerateOutput(inputPath, outputPath, settings); // Create the initial thumbnail
+
+                //ファイルが生成されない場合はパスを返さない
+                if (!System.IO.File.Exists(outputPath))
+                {
+                    imgFileName = "";
+                    outLogMsg(pdfFielName, "Pdf埋め込み画像なし");
+                } else
+                {
+                    outLogMsg(pdfFielName, "Pdf画像変換完了");
+                }
+            }
+            catch (Exception exp)
+            {
+                imgFileName = "";
+                outLogMsg(pdfFielName, "Pdf画像取得実施エラー : " + exp.Message);
+            }
+
+            return imgFileName;
+        }
+
+        //ログ情報を出力
+        private void outLogMsg(string fileName,  string message)
+        {
+            //画面ログに出力
+            string msg = fileName + " : " + message;
+
+            listBoxExpPdfImg.Items.Add(msg);
+            listBoxExpPdfImg.Update();
+        }
+    }
+}
